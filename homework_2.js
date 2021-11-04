a1b0ab3ca96e1a635dd2fedce776a905371b3648
Tatiana Parfenova <93682280+Sirenariel@users.noreply.github.com>
2021-11-04 05:13:18 +0300

    Add files via upload

---
+++ homework_2.js
@@ -0,0 +1,35 @@
+
+
+console.log("кот" > "код") //true
+
+console.log( "2" + 2 * "2") //24
+
+console.log(undefined == null) //true
+
+console.log(undefined === null) //false
+
+console.log(null == 0) //false
+
+console.log(2 > "3") // false
+
+console.log(null - false + true) //1
+
+console.log(1 / "l") //1
+
+console.log("2" * "3") //6
+
+console.log(4 + 5 + "O") //90
+
+console.log("l" + 4 + 5) //145
+
+console.log("4" - 2) //2
+
+console.log("4" - "4x") //NaN
+
+console.log(null == false) //false
+
+console.log(" -4 "/ 0 + 1) //infinity
+
+console.log(null + 1) //1
+
+console.log(undefined + null) //NaN
